676bc54e8b2394628bc0f60bb0aea672c2354fa8
Stephanie Ballard <user.name>
2020-02-12 09:44:37 -0800

    .

--- AnimalSpotter/Application/APIController.swift
+++ AnimalSpotter/Application/APIController.swift
@@ -19,10 +19,34 @@ class APIController {
     private let baseUrl = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
     
     // create function for sign up
-    
+    func signUp(with user: User, completion: @escaping (Error?) -> ()) {
+        //create endpoint - specifit URL
+        let signUpUrl = baseUrl.appendingPathComponent("users/signup")
+        //create a URLRequest from above
+        var request = URLRequest(url: signUpUrl)
+        
+        //Modify the request for post, add proper headers
+        request.httpMethod = HTTPMethod.post.rawValue
+                       //value, key, we are sending this to the server
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        //encode the user model to JSON, attach as reuqest body
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(user)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+            completion(error)
+            return
+        }
+ 
+        //set up data task and handle response
+    }
     // create function for sign in
     
     // create function for fetching all animal names
+    
+    //creat function for fetching animal details
     
     // create function to fetch image
 }
